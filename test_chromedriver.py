b08a1df391310b0427f6c3ea1c9f41359472aacb
Al Whatmough <al.whatmough@gmail.com>
2021-08-05 18:01:23 +1000

    Update test_chromedriver.py

--- test_chromedriver.py
+++ test_chromedriver.py
@@ -1,8 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+
+
+#################
+### Variables ###
+#################
+
+# Garmin Connect - Creds
+webAddress = "https://connect.garmin.com/"
+email = 'al.whatmough@gmail.com'
+password = os.environ.get("MY_SECRET")
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options)
+driver.get(webAddress)
 
-print(driver)
+def clickItem(xpathstr):
+    """ Click on a web element using selenium
+    params
+    ------
+    xpathstr: string
+        xPath of the web element
+    """
+    item = driver.find_element_by_xpath(xpathstr)
+    webdriver.ActionChains(driver).click(item).perform()
+
+# Click login button
+clickItem("""//*[@id="___gatsby"]/div/div/header/nav/ul/li[4]/a/button""")
+time.sleep(10)
+
+# Enter email - press tab
+webdriver.ActionChains(driver).send_keys(email).key_down(Keys.TAB).key_up(Keys.TAB).perform()
+time.sleep(1)
+
+# Enter password - press enter
+webdriver.ActionChains(driver).send_keys(password).key_down(Keys.ENTER).key_up(Keys.ENTER).perform()
+time.sleep(10)
+
+driver.get('https://connect.garmin.com/modern/activity/7224500180')
+
+time.sleep(10)
+
+print(driver.find_element_by_xpath("""//ul[contains(@class, 'gear-names')]/li[1]""").text)
