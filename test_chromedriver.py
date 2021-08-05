36e55639b18038fc0251caf562decc744c1b08d7
Al Whatmough <al.whatmough@gmail.com>
2021-08-05 19:41:30 +1000

    Update test_chromedriver.py

--- test_chromedriver.py
+++ test_chromedriver.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-import os
 import time
 
 
@@ -10,15 +9,15 @@ import time
 #################
 
 # Garmin Connect - Creds
-webAddress = "https://sso.garmin.com/sso/login?service=https%3A%2F%2Fconnect.garmin.com%2FminExplore&webhost=olaxpw-connect00&source=https%3A%2F%2Fconnect.garmin.com%2Fen-US%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_US&id=gauth-widget&cssUrl=https%3A%2F%2Fstatic.garmincdn.com%2Fcom.garmin.connect%2Fui%2Fcss%2Fgauth-custom-v1.1-min.css&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&usernameShown=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&generateExtraServiceTicket=false"
+webAddress = 'https://sso.garmin.com/sso/login?service=https%3A%2F%2Fconnect.garmin.com%2FminExplore&webhost=olaxpw-connect00&source=https%3A%2F%2Fconnect.garmin.com%2Fen-US%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_US&id=gauth-widget&cssUrl=https%3A%2F%2Fstatic.garmincdn.com%2Fcom.garmin.connect%2Fui%2Fcss%2Fgauth-custom-v1.1-min.css&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&usernameShown=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&generateExtraServiceTicket=false'
 email = 'al.whatmough@gmail.com'
 password = os.environ.get("MY_SECRET")
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome()
 driver.get(webAddress)
-print("HELLO")
 
 def clickItem(xpathstr):
     """ Click on a web element using selenium
@@ -26,22 +25,22 @@ def clickItem(xpathstr):
     ------
     xpathstr: string
         xPath of the web element
-    """
+    """ 
     item = driver.find_element_by_xpath(xpathstr)
     webdriver.ActionChains(driver).click(item).perform()
 
+# # Click login button
+# clickItem("""//*[@id="___gatsby"]/div/div/header/nav/ul/li[4]/a/button""")
+# time.sleep(10)
 time.sleep(10)
 
-print(driver.page_source.encode("utf-8"))
-    
-un = driver.find_element_by_id("username")
-pw = driver.find_element_by_id("password")
-un.send_keys(email)
-pw.send_keys(password)
-login_attempt = driver.find_element_by_xpath("//*[@type='submit']")
-login_attempt.submit()
+# Enter email - press tab
+webdriver.ActionChains(driver).send_keys(email).key_down(Keys.TAB).key_up(Keys.TAB).perform()
+time.sleep(1)
 
-print(driver.page_source.encode("utf-8"))
+# Enter password - press enter
+webdriver.ActionChains(driver).send_keys(password).key_down(Keys.ENTER).key_up(Keys.ENTER).perform()
+time.sleep(10)
 
 driver.get('https://connect.garmin.com/modern/activity/7224500180')
 
