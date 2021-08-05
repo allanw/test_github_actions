da2f4e5178d5d12a49d6289f6c262dc717bb5582
Al Whatmough <al.whatmough@gmail.com>
2021-08-05 14:19:28 +1000

    Create test_chromedriver.py

---
+++ test_chromedriver.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(options=chrome_options)
+
+print(driver)
